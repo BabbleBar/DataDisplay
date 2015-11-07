f95e115d2ac8b19e1ead8295bdd7ebac333e95e1
Florian Schrag <florian@schrag.org>
2015-11-07 14:42:37 +0100

    add DataDisplay

---
+++ README.md
@@ -0,0 +1,1 @@
+# DataDisplay Travis CI Status [![Build Status](https://travis-ci.org/BabbleBar/DataDisplay.svg?branch=master)](https://travis-ci.org/BabbleBar/DataDisplay)
---
+++ display.py
@@ -0,0 +1,62 @@
+from flask import Flask
+import os
+import pika
+import json
+import threading
+import pprint
+from pymongo import MongoClient
+import pymongo, render_template
+from flask.ext.socketio import SocketIO, emit
+
+app = Flask(__name__)
+
+
+def get_pika_params():
+    if 'VCAP_SERVICES' in os.environ:
+        vcap_service = json.loads(os.environ['VCAP_SERVICES'])
+
+        return pika.URLParameters(url=vcap_service['rabbitmq'][0]['credentials']['uri'])
+
+    return pika.ConnectionParameters(host="localhost")
+
+
+def receive_new_message(ch, method, properties, body):
+    data = json.loads(body)
+    print("EUI: %s %s - %s: %s" % (data['DevEUI'],
+                                   data['Time'],
+                                   data['FPort'],
+                                   data['payload_hex']
+                                   ))
+    print("michi does something :D")
+
+
+def start_listener():
+    channel = connection.channel()
+    channel.exchange_declare(exchange='data_log', type='fanout')
+    result = channel.queue_declare(exclusive=True)
+    queue_name = result.method.queue
+
+    channel.queue_bind(exchange='data_log',
+                       queue=queue_name)
+
+    print('listener started')
+
+    channel.basic_consume(receive_new_message,
+                          queue=queue_name,
+                          no_ack=True)
+
+    channel.start_consuming()
+
+
+
+socketio = SocketIO(app)
+
+if __name__ == "__main__":
+    port = os.getenv('VCAP_APP_PORT', '5000')
+
+    connection = pika.BlockingConnection(get_pika_params())
+    thread = threading.Thread(target=start_listener)
+    thread.setDaemon(True)
+    thread.start()
+
+    app.run(host='0.0.0.0', port=int(port), debug=True)
